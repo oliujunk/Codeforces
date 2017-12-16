e8e4eb727ba3a6b907d0335b02f46c3b8daaef55
oliujunk <413041277@qq.com>
2017-12-16 15:44:47 +0800

    accomplish 2A

--- 2_A.c
+++ 2_A.c
@@ -1,44 +1,52 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int n;
-    int i = 0;
+    int n, m;
+    int i = 0, j = 0, k = 0;
     struct nameSorceSt {
         char name[33];
         int sorce;
     };
 
     struct nameSorceSt nameSorce[1000] = {{0, 0}};
+    struct nameSorceSt nameSorce2[1000] = {{0, 0}};
 
-    char nameTemp[33];
-    int sorceTemp;
+    struct maxSorceSt {
+        int maxNameIndex;
+        int maxIndex;
+    };
+
     int emptyIndex = 0;
-    int maxArray[100] = {0};
+    struct maxSorceSt maxSorceArray[100] = {{0, 0}};
     int maxNum = 0;
     int maxSorce = 0;
 
     scanf("%d", &n);
 
+    m = n;
+
     while(n--)
     {
-        scanf("%s %d", nameTemp, &sorceTemp);
+        scanf("%s %d", nameSorce[m-n-1].name, &(nameSorce[m-n-1].sorce));
+        memcpy(&nameSorce2[m-n-1], &nameSorce[m-n-1], sizeof(nameSorce[m-n-1]));
         for(i=0; i<emptyIndex; i++)
         {
-            if(strcmp(nameSorce[i].name, nameTemp) == 0)
+            if(strcmp(nameSorce[i].name, nameSorce[m-n-1].name) == 0)
             {
-                nameSorce[i].sorce += sorceTemp;
+                nameSorce[i].sorce += nameSorce[m-n-1].sorce;
                 break;
             }
         }
         if(i == emptyIndex)
         {
-            strcpy(nameSorce[i].name, nameTemp);
-            nameSorce[i].sorce = sorceTemp;
+            strcpy(nameSorce[i].name, nameSorce[m-n-1].name);
+            nameSorce[i].sorce = nameSorce[m-n-1].sorce;
             emptyIndex++;
         }
-    }
+    }//计算出每个人的最终得分
 
     maxSorce = nameSorce[0].sorce;
 
@@ -48,20 +56,46 @@ int main(void)
         {
             maxSorce = nameSorce[i].sorce;
             maxNum = 1;
-            memset(maxArray, 0, sizeof(maxArray));
-            maxArray[0] = i;
+            memset(maxSorceArray, 0, sizeof(maxSorceArray));
+            maxSorceArray[0].maxNameIndex = i;
         }
         else if(nameSorce[i].sorce == maxSorce)
         {
-            maxArray[maxNum] = i;
+            maxSorceArray[maxNum].maxNameIndex = i;
             maxNum++;
         }
+    }//计算出最高分，并统计出得到最高分人的位置
+
+//    for(j=0; j<maxNum; j++)
+//    {
+//        for(i=m-1; i>=0; i--)
+//        {
+//            if(strcmp(nameSorce[i].name, nameSorce[maxSorceArray[j].maxNameIndex].name) == 0)
+//            {
+//                maxSorceArray[j].maxIndex = i;
+//                break;
+//            }
+//        }
+//    }//计算出最高分最后一次结算的位置
+
+    int tempSorce;
+
+    for(n=0; n<m; n++)
+    {
+        for(i=0; i<maxNum; i++)
+        {
+            if(strcmp(nameSorce2[n].name, nameSorce[maxSorceArray[i].maxNameIndex].name) == 0)
+            {
+                maxSorceArray[i].maxIndex += nameSorce2[n].sorce;
+                if(maxSorceArray[i].maxIndex >= maxSorce)
+                {
+                    printf("%s\n", nameSorce2[n].name);
+                    return 0;
+                }
+                break;
+            }
+        }
     }
-    strcpy(nameTemp, nameSorce[maxArray[0]].name);
-    for(i=1; i<maxNum; i++)
-        if(strcmp(nameTemp, nameSorce[maxArray[i]].name) >= 0)
-            strcpy(nameTemp, nameSorce[maxArray[i]].name);
-    printf("%s\n", nameTemp);
 
     return 0;
 }
