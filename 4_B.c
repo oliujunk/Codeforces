cdb6802035c9725e8f283b0bb967c62d4b49b9d2
oliujunk <413041277@qq.com>
2018-01-05 15:48:01 +0800

    accomplish 4B

--- 4_B.c
+++ 4_B.c
@@ -9,7 +9,7 @@ typedef struct
 int main(void)
 {
     int day, sumHour;
-    int i;
+    int i, j;
     int minSum = 0, maxSum = 0;
     StudyTimeSt studyTime[30];
     scanf("%d %d", &day, &sumHour);
@@ -28,7 +28,29 @@ int main(void)
     else
     {
         printf("YES\n");
-        
+        for (i = 0; i < day; i++)
+        {
+            if(minSum + studyTime[i].maxTime - studyTime[i].minTime >= sumHour)
+                break;
+            else
+                minSum += (studyTime[i].maxTime - studyTime[i].minTime);
+        }
+
+        if(i<day-1)
+        {
+            for(j=0; j<i; j++)
+                printf("%d ", studyTime[j].maxTime);
+            printf("%d ", studyTime[i].minTime+sumHour-minSum);
+            for(j=i+1; j<day-1; j++)
+                printf("%d ", studyTime[j].minTime);
+            printf("%d\n", studyTime[day-1].minTime);        
+        }
+        else
+        {
+            for(j=0; j<i; j++)
+                printf("%d ", studyTime[j].maxTime);
+            printf("%d\n", studyTime[day-1].minTime+sumHour-minSum);
+        }
         return 0;
     }
 }
